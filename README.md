12b6ee7f0ee1d0d362a9fa22b42d094e8ddd5d9c
Ibrahim Alharthi <ibrahim.almidfa@mbrsc.ae>
2018-04-15 00:08:53 -0600

    Creating Initial Coursera Repository

---
+++ README.md
@@ -0,0 +1,10 @@
+/******************************************************************************
+ * Copyright (C) 2018 by Ibrahim Alharthi - Mohammed Bin Rashid Space Centre
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Ibrahim Alharthi and MBRSC are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
---
+++ stats.h
@@ -0,0 +1,31 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file stats.h
+ * @brief Contains functions to perform statistics calculations
+ *
+ * The file contains a list of statistics functions to calculate Median, Mean
+ * Maximum, Minimum. Also, it has 2 functions to print the results from the 
+ * performed statistics calculations. Also, another print function to print 
+ * the array given. Finally, there is a sorting function.
+ *
+ * @author Ibrahim Alharthi
+ * @date 04/15/2018
+ *
+ */
+
+
+#ifndef __STATS_H__
+#define __STATS_H__
+
+
+
+#endif  /* __STATS_H__ */
